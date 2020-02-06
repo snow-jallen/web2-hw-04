c9c72a81abf69c2483bea9c03153f608650ce1f8
Jonathan Allen <jallen@snow.edu>
2020-02-06 10:04:44 -0700

    show config value

--- Web2Starter/Pages/Index.cshtml.cs
+++ Web2Starter/Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Web2Starter.Pages
@@ -11,15 +12,19 @@ namespace Web2Starter.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration config;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
         {
             _logger = logger;
+            this.config = config;
         }
 
         public void OnGet()
         {
 
         }
+
+        public string Endpoint => config["ENDPOINT"];
     }
 }
